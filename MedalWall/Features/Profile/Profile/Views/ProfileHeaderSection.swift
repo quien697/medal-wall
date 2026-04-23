21bfd42d00155fc2a81f955325c0bdc96ca5c56a
Quien <quien697@gmail.com>
2026-04-24 03:39:09 +0800

    feat: update cropping image flow and remove cropAvatarData in user model

--- MedalWall/Features/Profile/Profile/Views/ProfileHeaderSection.swift
+++ MedalWall/Features/Profile/Profile/Views/ProfileHeaderSection.swift
@@ -9,17 +9,13 @@ import SwiftUI
 
 struct ProfileHeaderSection: View {
   let avatar: UIImage?
-  let cropAvatar: UIImage?
   let userName: String
   let bio: String?
   
   var body: some View {
     SectionContainer {
       HStack(alignment: .top) {
-        AvatarImage(
-          photo: avatar,
-          cropPhoto: cropAvatar
-        )
+        AvatarImage(photo: avatar)
         
         VStack(alignment: .leading, spacing: 8) {
           Text("\(userName)")
@@ -43,9 +39,9 @@ struct ProfileHeaderSection: View {
 
 #Preview {
   VStack {
-    ProfileHeaderSection(avatar: UIImage(named: "quien"), cropAvatar: nil, userName: "Tsung-Hsun Liu", bio: "I am here to cross the finish line.")
+    ProfileHeaderSection(avatar: UIImage(named: "quien"), userName: "Tsung-Hsun Liu", bio: "I am here to cross the finish line.")
     
-    ProfileHeaderSection(avatar: nil, cropAvatar: nil, userName: "Tsung-Hsun Liu", bio: "Fike it till make it.")
+    ProfileHeaderSection(avatar: nil, userName: "Tsung-Hsun Liu", bio: "Fike it till make it.")
   }
   .background(Color.backgroundPrimary)
 }
