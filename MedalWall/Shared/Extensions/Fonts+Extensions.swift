8e550ceba25ee51dd3b3e2fcc91253302a870fd7
Quien <quien697@gmail.com>
2026-08-25 14:37:30 +0800

    [MW-29] feat: add typography scale

---
+++ MedalWall/Shared/Extensions/Fonts+Extensions.swift
@@ -0,0 +1,27 @@
+//
+//  Fonts+Extensions.swift
+//  MedalWall
+//
+//  Created by Quien on 2026-08-25.
+//
+
+import SwiftUI
+
+extension Font {
+  struct TypeScale {
+    static let display = Font.system(size: 32, weight: .black)
+    static let title1 = Font.system(size: 26, weight: .heavy)
+    static let title2 = Font.system(size: 20, weight: .bold)
+    static let headline = Font.system(size: 17, weight: .bold)
+    static let body = Font.system(size: 17, weight: .regular)
+    static let callout = Font.system(size: 15, weight: .medium)
+    static let caption = Font.system(size: 13, weight: .medium)
+    static let label = Font.system(size: 11, weight: .bold)
+
+    struct Numeric {
+      static let large = Font.system(size: 32, weight: .bold).monospacedDigit()
+      static let medium = Font.system(size: 20, weight: .bold).monospacedDigit()
+      static let small = Font.system(size: 15, weight: .semibold).monospacedDigit()
+    }
+  }
+}
